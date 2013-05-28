25e2d2c009b3178642b37790bbe52c8ea3140f47
Leonardo Danieli <leonardo.danieli+git@gmail.com>
2013-05-28 11:16:03 -0300

    Melhoria Carrinho,CarrinhoDAO

--- src/produtos/Carrinho.java
+++ src/produtos/Carrinho.java
@@ -2,4 +2,133 @@ package produtos;
 
 public class Carrinho {
 
+	private int idProduto;
+	private String titulo;
+	private String descricao;
+	private double preco;
+	private int qtdNoCarrinho;
+	private int idProdutoCarrinho;
+	private int qtdProdutos;
+	private int idCarrinho;
+	private String dataCadastroCarrinho;
+
+
+
+	public Carrinho() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public int getIdProduto() {
+		return idProduto;
+	}
+
+
+
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+	public double getPreco() {
+		return preco;
+	}
+
+
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+
+
+	public int getQtdNoCarrinho() {
+		return qtdNoCarrinho;
+	}
+
+
+
+	public void setQtdNoCarrinho(int qtdNoCarrinho) {
+		this.qtdNoCarrinho = qtdNoCarrinho;
+	}
+
+
+
+	public int getIdProdutoCarrinho() {
+		return idProdutoCarrinho;
+	}
+
+
+
+	public void setIdProdutoCarrinho(int idProdutoCarrinho) {
+		this.idProdutoCarrinho = idProdutoCarrinho;
+	}
+
+
+
+	public int getQtdProdutos() {
+		return qtdProdutos;
+	}
+
+
+
+	public void setQtdProdutos(int qtdProdutos) {
+		this.qtdProdutos = qtdProdutos;
+	}
+
+
+
+	public int getIdCarrinho() {
+		return idCarrinho;
+	}
+
+
+
+	public void setIdCarrinho(int idCarrinho) {
+		this.idCarrinho = idCarrinho;
+	}
+
+
+
+	public String getDataCadastroCarrinho() {
+		return dataCadastroCarrinho;
+	}
+
+
+
+	public void setDataCadastroCarrinho(String dataCadastroCarrinho) {
+		this.dataCadastroCarrinho = dataCadastroCarrinho;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Item do carrinho [id=%s, titulo=%s]", idCarrinho, titulo);
+	}
+
 }
--- src/produtos/CarrinhoDAO.java
+++ src/produtos/CarrinhoDAO.java
@@ -2,10 +2,19 @@ package produtos;
 
 import java.util.ArrayList;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CarrinhoDAO {
 
-	private static String PRODUTOS = "select * from produto where descricao ilike '%?%' or titulo ilike '%?%';";
-	private static String CARRINHO =
+	private static final String selectProdutos = "select * from produto where descricao ilike '%?%' or titulo ilike '%?%';";
+	private static final String selectCarrinho =
 		"SELECT "+
  " produto.id_produto, "+
  " produto.titulo, "+
@@ -22,22 +31,157 @@ public class CarrinhoDAO {
  " public.produtocarrinho "+
 " WHERE "+
 "  carrinho.id_carrinho = produtocarrinho.id_carrinho AND "+
-"  produto.id_produto = produtocarrinho.id_produto  " +
+"  produto.id_produto = produtocarrinho.id_produto and  " +
 "  carrinho.id_carrinho = ? ";
 
 
-	private static String CRIA_CARRINHO = "INSERT INTO carrinho(datacadastro) VALUES (  ?);";
-	private static String ADD_ITEM_CARRINHO = "INSERT INTO produtocarrinho(id_carrinho, id_produto, qtd)VALUES ( ?, ?, ?);";
+	private static final String criaCarrinho = "INSERT INTO carrinho(datacadastro) VALUES (  ?);";
+	private static final String insertProdutoCarrinho = "INSERT INTO produtocarrinho(id_carrinho, id_produto, qtd)VALUES ( ?, ?, ?);";
+
+	/**
+	 * Retorna um objeto com a lista de Produtos do carrinho com o <code>id</code> 
+	 * e com a senha <code>senha</code>.
+	 *
+	 * @param id_carrinho
+	 *            o id do carrinho, não pode ser null.
+	 *
+	 * @return null caso o id do carrinho não exista, caso contrário, retorna
+	 *         um objeto Carrinho com a lista de itens do carrinho
+	 *
+	 */
+	public List<Carrinho> selectCarrinho(Integer id_carrinho) {
+		if (id_carrinho == null) {
+			throw new IllegalArgumentException(
+					"O cod do Carrinho não pode ser null.");
+		}
+
+		Carrinho c = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		Connection con = null;
+		List<Carrinho> carrinho = new ArrayList<Carrinho>();
+		try {
+			con = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/produtos_lp2_leo", "postgres",
+					"senacrs");
+
+			stmt = con.prepareStatement(selectCarrinho);
+			stmt.setInt(1, id_carrinho);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				c = new Carrinho();
+				c.setIdProduto(rs.getInt("id_produto"));
+				c.setQtdNoCarrinho(rs.getInt("qtd_no_carrinho"));
+				c.setIdProdutoCarrinho(rs.getInt("id_produtocarrinho"));
+				c.setQtdProdutos(rs.getInt("qtd_produtos"));
+				c.setIdCarrinho(rs.getInt("id_carrinho"));
+				c.setTitulo(rs.getString("titulo"));
+				c.setDescricao(rs.getString("descricao"));
+				c.setPreco(rs.getDouble("preco"));
+				c.setDataCadastroCarrinho(rs.getString("datacadastrocarrinho"));
+
+				carrinho.add(c);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			// FIXME: comunicar erro ao programa cliente
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (rs != null) {
+					stmt.close();
+				}
+				if (con != null) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				// FIXME: comunicar erro ao programa cliente
+			}
+		}
+		return carrinho;
+	}
+	
+	/**
+	 * Retorna um objeto com a lista de Produtos do carrinho com o <code>id</code> 
+	 * e com a senha <code>senha</code>.
+	 *
+	 * @param id_carrinho
+	 *            o id do carrinho, não pode ser null.
+	 *
+	 * @return null caso o id do carrinho não exista, caso contrário, retorna
+	 *         um objeto Carrinho com a lista de itens do carrinho
+	 *
+	 */
+	public List<Carrinho> selectCarrinho(Integer id_carrinho) {
+		if (id_carrinho == null) {
+			throw new IllegalArgumentException(
+					"O cod do Carrinho não pode ser null.");
+		}
+
+		Carrinho c = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		Connection con = null;
+		List<Carrinho> carrinho = new ArrayList<Carrinho>();
+		try {
+			con = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/produtos_lp2_leo", "postgres",
+					"senacrs");
+
+			stmt = con.prepareStatement(selectCarrinho);
+			stmt.setInt(1, id_carrinho);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				c = new Carrinho();
+				c.setIdProduto(rs.getInt("id_produto"));
+				c.setQtdNoCarrinho(rs.getInt("qtd_no_carrinho"));
+				c.setIdProdutoCarrinho(rs.getInt("id_produtocarrinho"));
+				c.setQtdProdutos(rs.getInt("qtd_produtos"));
+				c.setIdCarrinho(rs.getInt("id_carrinho"));
+				c.setTitulo(rs.getString("titulo"));
+				c.setDescricao(rs.getString("descricao"));
+				c.setPreco(rs.getDouble("preco"));
+				c.setDataCadastroCarrinho(rs.getString("datacadastrocarrinho"));
+
+				carrinho.add(c);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			// FIXME: comunicar erro ao programa cliente
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (rs != null) {
+					stmt.close();
+				}
+				if (con != null) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				// FIXME: comunicar erro ao programa cliente
+			}
+		}
+		return carrinho;
+	}
 
 
-	public ArrayList<Produto> getProdutos(String consulta) {
-
-		//consultar do banco os produtos
-
-
-
-		return null;
-
+	public static void main(String[] args) {
+		CarrinhoDAO car = new CarrinhoDAO();
+	System.out.println(car.selectCarrinho(1));
+//		List<Carrinho> c = car.selectCarrinho("1");
+//		if (c == null) {
+//			System.out.println("Conta não encontrada!");
+//		} else {
+//			System.out.println(c);
+//			car.depositar(c, 112.45);
+//			System.out.println(car.consultarExtrato(c));
+//		}
 	}
 
 }
